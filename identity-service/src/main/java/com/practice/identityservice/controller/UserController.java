c51ce264f25a4ab12cedf28da97a94c1f31a5254
Sai Manohar <saimanohar91199@gmail.com>
2026-08-18 18:02:39 +0530

    add user service and 404 handling

--- identity-service/src/main/java/com/practice/identityservice/controller/UserController.java
+++ identity-service/src/main/java/com/practice/identityservice/controller/UserController.java
@@ -1,37 +1,27 @@
 package com.practice.identityservice.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.practice.identityservice.model.UserResponse;
+import com.practice.identityservice.dto.UserResponse;
+import com.practice.identityservice.service.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-	private final Map<Long, UserResponse> users = new HashMap<>();
+	private final UserService userService;
 
-	public UserController() {
-		users.put(1001L, new UserResponse(1001L, "Ananya Sharma", "ananya.sharma@company.com", "EMPLOYEE"));
-		users.put(1002L, new UserResponse(1002L, "Rohit Verma", "rohit.verma@company.com", "MANAGER"));
+	public UserController(UserService userService) {
+		this.userService = userService;
 	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<UserResponse> getUser(@PathVariable Long id) {
-		UserResponse user = users.get(id);
-
-		if (user == null) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-		}
-
-		return ResponseEntity.ok(user);
+		return ResponseEntity.ok(userService.getUser(id));
 	}
 
 }
---
+++ identity-service/src/main/java/com/practice/identityservice/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.practice.identityservice.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+
+}
